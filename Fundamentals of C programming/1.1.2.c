ad1cf820d7a6e1a6251fa5634e1a66c1f65bde03
consul-k <138245769+consul-k@users.noreply.github.com>
2023-07-01 21:59:49 +0300

    Update 1.1.2.c
    task condition added

--- Fundamentals of C programming/1.1.2.c
+++ Fundamentals of C programming/1.1.2.c
@@ -1,3 +1,28 @@
+/*
+
+Вы смотрели фильм «Матрица»? Если нет, то советую посмотреть. А задание будет следующим: переписать программу «Hello, world!» так, чтобы на экране появился следующий текст:
+
+Wake up, Neo...
+
+The Matrix has you...
+
+Follow the white rabbit...
+
+Подсказка: Вы можете использовать сколько угодно символов "\n". Строки с текстом разделены пустой строкой.
+
+Sample Input:
+
+
+Sample Output:
+
+Wake up, Neo...
+
+The Matrix has you...
+
+Follow the white rabbit...
+
+*/
+
 #include <stdio.h>
 
 int main() {
