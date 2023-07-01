3cac6f9310795c39db5b2dae49ab7805e9071ad6
consul-k <138245769+consul-k@users.noreply.github.com>
2023-07-01 22:00:27 +0300

    Update 1.1.3.c
    task condition added

--- Fundamentals of C programming/1.1.3.c
+++ Fundamentals of C programming/1.1.3.c
@@ -1,3 +1,26 @@
+/*
+
+Напишите программу, которая выводит на экран следующие строки:
+
+NICKNAME: Neo
+CITY: St-Petersburg
+AGE: 35
+HEIGHT: 180
+WEIGHT: 75
+
+Sample Input:
+
+
+Sample Output:
+
+NICKNAME: Neo
+CITY: St-Petersburg
+AGE: 35
+HEIGHT: 180
+WEIGHT: 75
+
+*/
+
 #include <stdio.h>
 
 int main() {
