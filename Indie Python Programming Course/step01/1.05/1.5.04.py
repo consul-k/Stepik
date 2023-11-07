b40ae7050345ef986ebe03acb247ace05476d941
consul-k <138245769+consul-k@users.noreply.github.com>
2023-11-07 14:28:23 +0300

    Create 1.5.04.py

---
+++ Indie Python Programming Course/step01/1.05/1.5.04.py
@@ -0,0 +1,36 @@
+'''
+
+Напишите программу, которая принимает на вход два целых числа в отдельных строках и выводит на экран их сумму.
+
+Sample Input 1:
+
+7
+5
+
+Sample Output 1:
+
+12
+
+Sample Input 2:
+
+100
+200
+
+Sample Output 2:
+
+300
+
+Sample Input 3:
+
+3119
+3942
+
+Sample Output 3:
+
+7061
+
+'''
+
+a = int(input())
+b = int(input())
+print(a+b)
