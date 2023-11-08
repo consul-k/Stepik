0c5019c35f96a5f76c8b59f0168907e8d7a06047
consul-k <138245769+consul-k@users.noreply.github.com>
2023-11-08 14:18:32 +0300

    Create 1.8.05.py

---
+++ Indie Python Programming Course/step01/1.08/1.8.05.py
@@ -0,0 +1,31 @@
+'''
+
+Дано целое положительное число, ваша задача вывести разряд сотен этого числа
+
+Sample Input 1:
+
+123
+
+Sample Output 1:
+
+1
+
+Sample Input 2:
+
+56789
+
+Sample Output 2:
+
+7
+
+Sample Input 3:
+
+54
+
+Sample Output 3:
+
+0
+
+'''
+
+print(int(input())//100%10)
