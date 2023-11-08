2376cd716f082e3d10d4422c416f8dba8cda6cd5
consul-k <138245769+consul-k@users.noreply.github.com>
2023-11-08 15:59:11 +0300

    Create 1.10.09.py

---
+++ Indie Python Programming Course/step01/1.10/1.10.09.py
@@ -0,0 +1,30 @@
+'''
+
+На вход поступают два слова.
+
+Программа должна вывести True, если хотя бы одно слово равно слову "awesome".
+
+Сделать задачу необходимо без использования условного оператора.
+
+Sample Input 1:
+
+cool
+awesome
+
+Sample Output 1:
+
+True
+
+Sample Input 2:
+
+aaa
+aaa
+
+Sample Output 2:
+
+False
+
+'''
+
+a,b = input(), input()
+print(a=='awesome' or b=='awesome')
