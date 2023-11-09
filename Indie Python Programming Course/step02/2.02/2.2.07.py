751f185aa0a74b0bc1a7e8a5f92b073ebffee799
consul-k <138245769+consul-k@users.noreply.github.com>
2023-11-09 14:21:53 +0300

    Create 2.2.07.py

---
+++ Indie Python Programming Course/step02/2.02/2.2.07.py
@@ -0,0 +1,15 @@
+'''
+
+Программа получает на вход строку. Ваша задача развернуть строку и вывести ее на экран.
+
+Sample Input:
+
+leetcode.com
+
+Sample Output:
+
+moc.edocteel
+
+'''
+
+print(input()[::-1])
