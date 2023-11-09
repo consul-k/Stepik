ea812a2dbd121c4d1fe744bc9a062b44220141f9
consul-k <138245769+consul-k@users.noreply.github.com>
2023-11-09 14:13:54 +0300

    Create 2.2.01.py

---
+++ Indie Python Programming Course/step02/2.02/2.2.01.py
@@ -0,0 +1,23 @@
+'''
+
+Программа получает на вход строку и ваша задача вывести первый элемент данной строки
+
+Sample Input 1:
+
+Zemfira
+
+Sample Output 1:
+
+Z
+
+Sample Input 2:
+
+It
+
+Sample Output 2:
+
+I
+
+'''
+
+print(input()[0])
