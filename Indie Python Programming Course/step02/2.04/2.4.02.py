ccb903fec58f0f3e5db4c754d38d4597f04bc593
consul-k <138245769+consul-k@users.noreply.github.com>
2023-11-09 18:57:37 +0300

    Create 2.4.02.py

---
+++ Indie Python Programming Course/step02/2.04/2.4.02.py
@@ -0,0 +1,14 @@
+'''
+
+Ниже перед вами представлен список list_strings, состоящий из строк. При помощи метода .join и соединителя - получите строку из этих элементов и выведите ее на экран
+
+Sample Input:
+
+Sample Output:
+
+Follow-the-Cops-Back-Home
+
+'''
+
+list_strings = ['Follow', 'the', 'Cops', 'Back', 'Home']
+print('-'.join(list_strings))
