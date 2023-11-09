64343a2ba06fbd00bd885278aadd59c598c451a8
consul-k <138245769+consul-k@users.noreply.github.com>
2023-11-09 14:52:54 +0300

    Create 2.3.10.py

---
+++ Indie Python Programming Course/step02/2.03/2.3.10.py
@@ -0,0 +1,25 @@
+'''
+
+Программа получает на вход фразу, состоящую из нескольких слов, разделенных пробелом.
+
+Ваша задача заменить все пробелы запятыми и вывести полученную строку.
+
+Sample Input 1:
+
+Smells Like Teen Spirit
+
+Sample Output 1:
+
+Smells,Like,Teen,Spirit
+
+Sample Input 2:
+
+a million little pieces
+
+Sample Output 2:
+
+a,million,little,pieces
+
+'''
+
+print(input().replace(' ',','))
