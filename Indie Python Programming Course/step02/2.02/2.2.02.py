2f8ebd282f28dd9cd24ed3f035eb60273cda1b95
consul-k <138245769+consul-k@users.noreply.github.com>
2023-11-09 14:15:17 +0300

    Create 2.2.02.py

---
+++ Indie Python Programming Course/step02/2.02/2.2.02.py
@@ -0,0 +1,23 @@
+'''
+
+Программа получает на вход строку и ваша задача вывести последний символ этой строки
+
+Sample Input 1:
+
+Сплин
+
+Sample Output 1:
+
+н
+
+Sample Input 2:
+
+Billie Eilish
+
+Sample Output 2:
+
+h
+
+'''
+
+print(input()[-1])
