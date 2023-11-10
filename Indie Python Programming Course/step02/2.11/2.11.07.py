1b9083a92863a870b018c74a250c2cc012d83e95
consul-k <138245769+consul-k@users.noreply.github.com>
2023-11-10 13:53:52 +0300

    Create 2.11.07.py

---
+++ Indie Python Programming Course/step02/2.11/2.11.07.py
@@ -0,0 +1,30 @@
+'''
+
+Программа получает на вход список из целых чисел. Ваша задача найти среднее арифметическое введенного списка чисел
+
+Примечание:
+
+Чтобы прочитать из ввода целые числа и сохранить их в виде списка в переменной list_numbers вам необходимо написать строчку
+
+list_numbers = list(map(int, input().split()))
+
+Sample Input 1:
+
+8 10
+
+Sample Output 1:
+
+9.0
+
+Sample Input 2:
+
+1 2 3
+
+Sample Output 2:
+
+2.0
+
+'''
+
+list_numbers = list(map(int, input().split()))
+print(sum(list_numbers)/len(list_numbers))
