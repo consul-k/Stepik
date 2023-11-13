e72dc82731bd1f94ce77a23202939c508bc60d46
consul-k <138245769+consul-k@users.noreply.github.com>
2023-11-13 15:37:15 +0300

    Create 4.5.01.py

---
+++ Indie Python Programming Course/step04/4.05/4.5.01.py
@@ -0,0 +1,29 @@
+'''
+
+Даны два натуральных числа A и B. Требуется найти их наибольший общий делитель (НОД) методом вычитания
+
+Sample Input 1:
+
+77 22
+
+Sample Output 1:
+
+11
+
+Sample Input 2:
+
+5 7
+
+Sample Output 2:
+
+1
+
+'''
+
+a, b = map(int, input().split())
+while a != b:
+    if a > b:
+        a = a - b
+    else:
+        b = b - a
+print(a)
