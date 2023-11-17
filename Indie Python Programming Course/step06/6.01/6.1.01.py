0cffd99b58dc0fd8d15d64e1d7ff0ba7130de788
consul-k <138245769+consul-k@users.noreply.github.com>
2023-11-17 16:22:10 +0300

    Create 6.1.01.py

---
+++ Indie Python Programming Course/step06/6.01/6.1.01.py
@@ -0,0 +1,19 @@
+'''
+
+Создайте две переменные empty и empty_too, сохраните в них значение None
+
+При помощи оператора is выведите на первой строке результат их сравнения на равенство и затем на второй строке результат их сравнения на неравенсто
+
+Sample Input:
+
+Sample Output:
+
+True
+False
+
+'''
+
+empty = None
+empty_too = None
+print(None is None)
+print(None is not None)
