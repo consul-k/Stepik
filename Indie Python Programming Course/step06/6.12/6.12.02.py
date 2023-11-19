89c40d919f639235d19f7aec85b1ab7d95a1360d
consul-k <138245769+consul-k@users.noreply.github.com>
2023-11-19 19:24:35 +0300

    Create 6.12.02.py

---
+++ Indie Python Programming Course/step06/6.12/6.12.02.py
@@ -0,0 +1,28 @@
+'''
+
+Перед вами располагается множество  my_set 
+
+Ваша задача удалить из него 3 строковых элемента: 
+
+    government
+    national
+    tease
+
+Выводить ничего не нужно, только удалить элементы выше
+
+'''
+
+my_set = {
+    'mention', 'soup', 'pneumonia', 'tradition', 'concert', 'tease', 'generation',
+    'winter', 'national', 'jacket', 'winter', 'wrestle', 'proposal', 'error',
+    'pneumonia', 'concert', 'value', 'value', 'disclose', 'glasses', 'tank',
+    'national', 'soup', 'feel', 'few', 'concert', 'wrestle', 'proposal', 'soup',
+    'sail', 'brown', 'service', 'proposal', 'winter', 'jacket', 'mention',
+    'tradition', 'value', 'feel', 'bear', 'few', 'value', 'winter', 'proposal',
+    'government', 'control', 'value', 'few', 'generation', 'service', 'national',
+    'tradition', 'government', 'mention', 'proposal'
+}
+
+my_set.remove('government')
+my_set.remove('national')
+my_set.remove('tease')
