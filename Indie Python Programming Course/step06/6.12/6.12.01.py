6ae8590a33dd37f89ebf1c4acb61a4ccac4a4cb7
consul-k <138245769+consul-k@users.noreply.github.com>
2023-11-19 19:19:48 +0300

    Create 6.12.01.py

---
+++ Indie Python Programming Course/step06/6.12/6.12.01.py
@@ -0,0 +1,19 @@
+'''
+
+Перед вами располагается множество  my_set 
+
+Ваша задача добавить в него 4 строковых элемента: 
+
+    concert
+    brown
+    jacket
+    value
+
+Выводить ничего не нужно, только добавьте элементы выше
+
+'''
+
+my_set = {'government', 'control', 'winter', 'few', 'generation',
+          'service', 'national', 'tradition', 'government'}
+a = ('concert','brown','jacket','value')
+my_set.update(a)
