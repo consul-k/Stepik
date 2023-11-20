90f0fecc1c215dd65ea18f96f38028289f6f90d7
consul-k <138245769+consul-k@users.noreply.github.com>
2023-11-20 16:48:22 +0300

    Create 7.2.02.py

---
+++ Indie Python Programming Course/step07/7.02/7.2.02.py
@@ -0,0 +1,50 @@
+'''
+
+Напишите функцию is_between, которая принимает три аргумента и печатает True, если первое число находится между двумя вторыми включительно, 
+и False в противном случае.
+
+Ваша задача дописать только тело функции is_between
+
+Sample Input 1:
+
+5 3 6
+
+Sample Output 1:
+
+True
+
+Sample Input 2:
+
+10 2 5
+
+Sample Output 2:
+
+False
+
+Sample Input 3:
+
+9 9 9
+
+Sample Output 3:
+
+True
+
+Sample Input 4:
+
+5 9 1
+
+Sample Output 4:
+
+True
+
+'''
+
+def is_between(name, surname, middlename):
+    if surname <= name <= middlename or surname >= name >= middlename:
+        print(True)
+    else:
+        print(False)
+
+a, b, c = map(int, input().split())
+
+is_between(a, b, c)
