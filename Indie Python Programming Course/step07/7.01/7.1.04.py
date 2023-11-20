2a4c25e3d7687fce78b27262d6a26cd5c98ed101
consul-k <138245769+consul-k@users.noreply.github.com>
2023-11-20 16:06:20 +0300

    Create 7.1.04.py

---
+++ Indie Python Programming Course/step07/7.01/7.1.04.py
@@ -0,0 +1,34 @@
+'''
+
+Напишите функцию exponentiation, которая принимает на вход целое число и выводит на экран через пробел квадрат и куб этого числа. 
+
+Вам необходимо написать только определение функции exponentiation
+
+Sample Input 1:
+
+3
+
+Sample Output 1:
+
+9 27
+
+Sample Input 2:
+
+2
+
+Sample Output 2:
+
+4 8
+
+Sample Input 3:
+
+10
+
+Sample Output 3:
+
+100 1000
+
+'''
+
+def exponentiation(n):
+    print(n**2,n**3)
