1fc1cc37e52b8fceae7c6b27f43588fcfa611a8e
consul-k <138245769+consul-k@users.noreply.github.com>
2023-11-20 16:01:46 +0300

    Create 7.1.02.py

---
+++ Indie Python Programming Course/step07/7.01/7.1.02.py
@@ -0,0 +1,27 @@
+'''
+
+Всеми любимая программа «hello world». Создайте функцию с именем say_hello_world , 
+которая принимает на вход имя человека в виде строки и печатает фразу «{name} говорит hello world!»
+
+Ваша задача написать только определение функции say_hello_world 
+
+Sample Input 1:
+
+Jim Kerry
+
+Sample Output 1:
+
+Jim Kerry говорит hello world!
+
+Sample Input 2:
+
+Лунтик
+
+Sample Output 2:
+
+Лунтик говорит hello world!
+
+'''
+
+def say_hello_world(name):
+    print(f'{name} говорит hello world!')
