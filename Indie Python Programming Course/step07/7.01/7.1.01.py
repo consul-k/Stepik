55cbb92169b24713e4b83dadb858fb556d101648
consul-k <138245769+consul-k@users.noreply.github.com>
2023-11-20 16:00:03 +0300

    Create 7.1.01.py

---
+++ Indie Python Programming Course/step07/7.01/7.1.01.py
@@ -0,0 +1,18 @@
+'''
+
+Давайте начнем с легкого примера. Создадим функцию с именем keanu_reeves, которая выводит сообщение
+
+"YOU'RE BREATHTAKING"
+
+Ваша задача написать только определение функции keanu_reeves
+
+Sample Input:
+
+Sample Output:
+
+YOU'RE BREATHTAKING
+
+'''
+
+def keanu_reeves():
+    print("YOU'RE BREATHTAKING")
