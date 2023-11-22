da8d8a24c9c1dc9e010f00b7546ec4bc11e8b3a1
consul-k <138245769+consul-k@users.noreply.github.com>
2023-11-22 14:49:08 +0300

    Create 7.3.07.py

---
+++ Indie Python Programming Course/step07/7.03/7.3.07.py
@@ -0,0 +1,49 @@
+'''
+
+Напишите функцию first_unique_char, которая принимает строку символов и возвращает целое число: позицию первого уникального символа в строке. 
+В случае, если уникальных символов в переданной строке нет, верните -1. Регистр символов не учитывайте.
+
+Ваша задача написать только определение функции first_unique_char
+
+Sample Input 1:
+
+python
+
+Sample Output 1:
+
+0
+
+Sample Input 2:
+
+abracadabra
+
+Sample Output 2:
+
+4
+
+Sample Input 3:
+
+abcabc
+
+Sample Output 3:
+
+-1
+
+Sample Input 4:
+
+aasssddddddddq
+
+Sample Output 4:
+
+13
+
+'''
+
+def first_unique_char(s):
+    for char in s:
+        if s.count(char) == 1:
+            return s.index(char)
+    return -1
+
+s = input()
+print(first_unique_char(s))
