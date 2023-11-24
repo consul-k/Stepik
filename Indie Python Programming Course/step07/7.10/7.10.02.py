f9e42a32bc9330332ebf9e156c8f5a592dd95671
consul-k <138245769+consul-k@users.noreply.github.com>
2023-11-24 12:26:07 +0300

    Create 7.10.02.py

---
+++ Indie Python Programming Course/step07/7.10/7.10.02.py
@@ -0,0 +1,33 @@
+'''
+
+В переменную starts_with присвойте lambda функцию, которая принимает строку и возвращает True, когда переданная строка начинается с буквы W. Во всех остальных случаях нужно возвращать False
+
+Ничего кроме создания переменной starts_with делать не нужно
+
+Sample Input 1:
+
+World
+
+Sample Output 1:
+
+True
+
+Sample Input 2:
+
+when
+
+Sample Output 2:
+
+False
+
+Sample Input 3:
+
+Сурикаты
+
+Sample Output 3:
+
+False
+
+'''
+
+starts_with = lambda x: x[0] == 'W'
