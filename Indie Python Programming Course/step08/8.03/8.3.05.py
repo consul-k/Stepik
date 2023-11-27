f4e8b6eed5bdc7d3519b62dd538db5c7e05ba602
consul-k <138245769+consul-k@users.noreply.github.com>
2023-11-27 13:39:55 +0300

    Create 8.3.05.py

---
+++ Indie Python Programming Course/step08/8.03/8.3.05.py
@@ -0,0 +1,17 @@
+'''
+
+Из модуля string импортируйте следующие переменные: 
+
+    ascii_lowercase - строка, содержащая английский буквы англ. алфавита в нижнем регистре
+    ascii_uppercase - строка, содержащая английский буквы англ. алфавита в верхнем регистре
+    punctuation - строка, содержащая символы пунктуации
+
+Необходимо в отдельных строках вывести сперва все знаки пунктуации, затем заглавные символы и уже потом маленькие.
+
+'''
+
+from string import ascii_lowercase, ascii_uppercase, punctuation
+
+print(punctuation)
+print(ascii_uppercase)
+print(ascii_lowercase)
