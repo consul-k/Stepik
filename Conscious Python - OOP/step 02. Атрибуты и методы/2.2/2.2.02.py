edce26951d81519b84722811e4038c770315e822
consul-k <138245769+consul-k@users.noreply.github.com>
2023-12-26 14:40:17 +0300

    Create 2.2.02.py

---
+++ Conscious Python - OOP/step 02. Атрибуты и методы/2.2/2.2.02.py
@@ -0,0 +1,24 @@
+'''
+
+Задание:
+
+    Создайте пустой класс Сoordinate (используйте pass).
+    Создайте экземпляр coord класса Сoordinate.
+    Создайте атрибут "x" в экземпляре coord, со значением 100.
+    Измените атрибут "x"  в экземпляре coord на число 5.
+    Выведите на экран значение атрибута "x" экземпляра coord, с помощью print.
+
+Sample Input:
+
+Sample Output:
+
+5
+
+'''
+
+class Coordinate:
+    pass
+coord = Coordinate()
+coord.x = 100
+coord.x = 5
+print(coord.x)
