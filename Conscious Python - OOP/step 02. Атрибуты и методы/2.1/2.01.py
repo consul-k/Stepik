32ace31a1d4f99c8d451a49c1ba9c055dd4a1915
consul-k <138245769+consul-k@users.noreply.github.com>
2023-12-26 14:37:48 +0300

    Create 2.01.py

---
+++ Conscious Python - OOP/step 02. Атрибуты и методы/2.1/2.01.py
@@ -0,0 +1,19 @@
+'''
+
+Задание:
+
+    Создайте класс Car.
+    Создайте атрибут color внутри класса, со значением "blue"
+    Выведите на экран значение атрибута color с помощью print()
+
+Sample Input:
+
+Sample Output:
+
+blue
+
+'''
+
+class Car:
+    color = 'blue'
+print(Car.color)
