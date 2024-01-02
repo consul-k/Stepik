aed1ae51f1f5fe7cab281e10d9bc0d3a138c0218
consul-k <138245769+consul-k@users.noreply.github.com>
2024-01-02 14:41:53 +0300

    Create 3.1.01.py

---
+++ Good, good Python/step 03/3.01/3.1.01.py
@@ -0,0 +1,17 @@
+'''
+
+Подвиг 5. Напишите программу ввода двух строк (каждая вводится с новой строки) и их объединения в одну строку через пробел. Результат выведите на экран.
+
+Sample Input:
+
+hello python
+i love you
+
+Sample Output:
+
+hello python i love you
+
+'''
+
+a, b = input(), input()
+print(f'{a} {b}')
