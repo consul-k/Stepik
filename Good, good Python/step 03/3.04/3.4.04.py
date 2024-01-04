a70f9d53628c3d585ed1a2378ac27799e5978624
consul-k <138245769+consul-k@users.noreply.github.com>
2024-01-05 00:04:36 +0300

    Create 3.4.04.py

---
+++ Good, good Python/step 03/3.04/3.4.04.py
@@ -0,0 +1,13 @@
+'''
+
+Подвиг 5. Используя raw-строки, задайте строку, содержащую этот путь к файлу: C:\WINDOWS\System32\drivers\etc\hosts. Результат отобразите на экране.
+
+Sample Input:
+
+Sample Output:
+
+C:\WINDOWS\System32\drivers\etc\hosts
+
+'''
+
+print(r'C:\WINDOWS\System32\drivers\etc\hosts')
