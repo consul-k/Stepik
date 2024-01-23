1d2c1b858097582ffffb468ac845c6cd2e35a8df
consul-k <138245769+consul-k@users.noreply.github.com>
2024-01-23 12:53:51 +0300

    Create 7.1.01.py

---
+++ Good, good Python/step 07/7.01/7.1.01.py
@@ -0,0 +1,20 @@
+'''
+
+Подвиг 3. Задайте функцию, которая не принимает никаких аргументов и просто выводит на экран строку:
+
+It's my first function
+
+В конце программы вызовите эту функцию.
+
+Sample Input:
+
+Sample Output:
+
+It's my first function
+
+'''
+
+def hi():
+    print('It\'s my first function')
+
+hi()
