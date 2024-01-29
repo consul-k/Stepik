ea6e728e471bdeef21e4264733c244e74def45ca
consul-k <138245769+consul-k@users.noreply.github.com>
2024-01-29 14:14:39 +0300

    Create 8.6.01.py

---
+++ Good, good Python/step 08/8.06/8.6.01.py
@@ -0,0 +1,13 @@
+'''
+
+Подвиг 2. Имеется фрагмент программы (см. листинг ниже). При его выполнении возникает ошибка FileNotFoundError. 
+Запишите конструкцию try / except, чтобы отображалось сообщение "File Not Found", если файл не удается открыть.
+
+'''
+
+try:
+    f = open("abc.txt")
+    r = f.read(1)
+    f.close()
+except FileNotFoundError:
+    print('File Not Found')
