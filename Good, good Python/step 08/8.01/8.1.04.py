e98c6f1e1d361e5c40a65aad907af9770efd7611
consul-k <138245769+consul-k@users.noreply.github.com>
2024-01-29 14:13:22 +0300

    Create 8.1.04.py

---
+++ Good, good Python/step 08/8.01/8.1.04.py
@@ -0,0 +1,14 @@
+'''
+
+Подвиг 5. Из модуля random импортируйте только две функции: seed и randint. Затем, в программе выполните их, следующим образом:
+
+seed(1)
+print(randint(10, 50))
+
+'''
+
+# put your python code here
+from random import seed, randint
+
+seed(1)
+print(randint(10, 50))
