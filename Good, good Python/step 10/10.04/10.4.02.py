ce8275c4f2e6d2940e5ad7a9583aeb2ab46587a3
consul-k <138245769+consul-k@users.noreply.github.com>
2024-02-04 13:46:47 +0300

    Create 10.4.02.py

---
+++ Good, good Python/step 10/10.04/10.4.02.py
@@ -0,0 +1,23 @@
+'''
+
+Подвиг 6. В функцию get_data() передается параметр value:
+
+def get_data(value):
+    match value:
+        # здесь продолжайте программу
+    
+    return None
+
+Необходимо дописать оператор match/case в этой функции так, чтобы для каждого типа данных (int, float и str) 
+формировалась переменная со значением value и возвращалась функцией (непосредственно из блока case).
+
+P. S. Вызывать функцию не нужно, только дописать.
+
+'''
+
+def get_data(value):
+    match value:
+        case str() | int() | float():
+            return value
+
+    return None
