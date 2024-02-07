0e080e1a890e1ba31595c5a1c62575e45e85d481
consul-k <138245769+consul-k@users.noreply.github.com>
2024-02-07 15:45:10 +0300

    Create 2.02.py

---
+++ Adaptive Python Trainer/ 2. Excercises from "Python for Informatics" by Charles Severance/2.02.py
@@ -0,0 +1,15 @@
+'''
+
+Write a program that uses input to prompt a user for their name and then welcomes them.
+
+Enter your name: Chuck
+Hello Chuck
+
+Для считывания значений с клавиатуры в Python 3 используется функция input.
+
+В этой задаче ожидается использование функции input с аргументом.
+
+'''
+
+name = input('Enter your name: ')
+print(f'Hello {name}')
