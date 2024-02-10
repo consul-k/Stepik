4e7c7012b37adfc944d7b9523cbb58499480d14f
consul-k <138245769+consul-k@users.noreply.github.com>
2024-02-10 16:04:15 +0300

    Create 2.1.02.js

---
+++ Diving into JavaScript: For Beginners/step02/2.01/2.1.02.js
@@ -0,0 +1,14 @@
+/*
+
+Выведите слово "JavaScript" и напишите однострочный комментарий, объясняющий написанную вами инструкцию.
+
+Sample Input:
+
+
+Sample Output:
+
+JavaScript
+
+*/
+
+console.log('JavaScript') //print 'JavaScript'
