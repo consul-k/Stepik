e3bf8852ca879128c624c3de5fb5e484e9b91001
consul-k <138245769+consul-k@users.noreply.github.com>
2024-02-11 13:53:23 +0300

    Create 2.2.02.js

---
+++ Diving into JavaScript: For Beginners/step02/2.02/2.2.02.js
@@ -0,0 +1,16 @@
+/*
+
+Дополните код так, чтобы программа выводила значения переменных num1, num2 и num3 в заявленном порядке (см. Sample Output) через пробел.
+
+Sample Input:
+
+Sample Output:
+
+100 120 180
+
+*/
+
+let num1 = 180;
+let num2 = 100;
+let num3 = 120;
+console.log(num2, num3, num1);
