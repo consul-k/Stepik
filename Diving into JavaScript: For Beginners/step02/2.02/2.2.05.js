dc0dedd5df4c33639161429225d994f2c4d75352
consul-k <138245769+consul-k@users.noreply.github.com>
2024-02-11 13:54:58 +0300

    Create 2.2.05.js

---
+++ Diving into JavaScript: For Beginners/step02/2.02/2.2.05.js
@@ -0,0 +1,23 @@
+/*
+
+Создайте две переменные source и destination. Присвойте переменной source значение "sigma". 
+Затем скопируйте значение переменной source в переменную destination. Измените значение source на "delta". 
+Выведите значениe source, a затем значение destination с новой строки.
+
+Sample Input:
+
+
+Sample Output:
+
+delta
+sigma
+
+*/
+
+let source;
+let destination;
+source = 'sigma';
+destination = source;
+source = 'delta';
+console.log(source);
+console.log(destination);
