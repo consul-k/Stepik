b0638c52a6ee45bdbcda65855e9534ed30ca56ad
consul-k <138245769+consul-k@users.noreply.github.com>
2024-02-11 13:52:48 +0300

    Create 2.2.01.js

---
+++ Diving into JavaScript: For Beginners/step02/2.02/2.2.01.js
@@ -0,0 +1,16 @@
+/*
+
+Дополните код так, чтобы программа выводила сумму значений переменных num1, num2 и num3. 
+
+Sample Input:
+
+Sample Output:
+
+400
+
+*/
+
+let num1 = 180;
+let num2 = 100;
+let num3 = 120;
+console.log(num1 + num2 + num3);
