b4da1c8c627c303999d2ef6df220501a1461baaa
consul-k <138245769+consul-k@users.noreply.github.com>
2024-02-12 14:46:05 +0300

    Create 2.5.04.js

---
+++ Diving into JavaScript: For Beginners/step02/2.05/2.5.04.js
@@ -0,0 +1,32 @@
+/*
+
+Запросите у пользователя число. Вычислите остаток от деления этого числа на 4 и выведите результат.
+
+Sample Input 1:
+
+8
+
+Sample Output 1:
+
+0
+
+Sample Input 2:
+
+19
+
+Sample Output 2:
+
+3
+
+Sample Input 3:
+
+5
+
+Sample Output 3:
+
+1
+
+*/
+
+let a = Number(prompt());
+console.log(a%4);
