ee05e57e408bdf4062ea1cbcfa81c1abdfc0000e
consul-k <138245769+consul-k@users.noreply.github.com>
2024-02-12 15:30:51 +0300

    Create 2.6.02.js

---
+++ Diving into JavaScript: For Beginners/step02/2.06/2.6.02.js
@@ -0,0 +1,28 @@
+/*
+
+Запросите у пользователя число x, а затем число y. Проверьте x и у на строгое неравенство. Выведите результат проверки (true или false). 
+
+Sample Input 1:
+
+5
+4
+
+Sample Output 1:
+
+true
+
+Sample Input 2:
+
+8
+8
+
+Sample Output 2:
+
+false
+
+*/
+
+let x = Number(prompt());
+let y = Number(prompt());
+
+console.log(x !== y);
