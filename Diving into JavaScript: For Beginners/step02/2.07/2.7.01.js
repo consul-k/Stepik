dd1fc7018b772e9841f6b4a24412bcd8b9c711c5
consul-k <138245769+consul-k@users.noreply.github.com>
2024-02-12 15:32:34 +0300

    Create 2.7.01.js

---
+++ Diving into JavaScript: For Beginners/step02/2.07/2.7.01.js
@@ -0,0 +1,15 @@
+/*
+
+Вставьте вместо троеточия логический оператор так, чтобы константе logic было присвоено значение true.
+
+Sample Input:
+
+
+Sample Output:
+
+true
+
+*/
+
+const logic = false && 5 > 9 || 18 == "18"; 
+console.log(logic);
