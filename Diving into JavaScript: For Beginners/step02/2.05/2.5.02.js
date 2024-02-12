3d1e58b0c5db5fc246af97764a517a658360bbd6
consul-k <138245769+consul-k@users.noreply.github.com>
2024-02-12 14:44:46 +0300

    Create 2.5.02.js

---
+++ Diving into JavaScript: For Beginners/step02/2.05/2.5.02.js
@@ -0,0 +1,37 @@
+/*
+
+Запросите у пользователя число а и число b. Вычислите результат возведения числа a в степень b и выведите результат.
+
+Sample Input 1:
+
+2
+5
+
+Sample Output 1:
+
+32
+
+Sample Input 2:
+
+9
+2
+
+Sample Output 2:
+
+81
+
+Sample Input 3:
+
+3
+3
+
+Sample Output 3:
+
+27
+
+*/
+
+let a = Number(prompt());
+let b = Number(prompt());
+
+console.log(a**b);
