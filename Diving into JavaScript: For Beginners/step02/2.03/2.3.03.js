93253cf81fab0fe92b1ec3e92fa24a6f9c73bca6
consul-k <138245769+consul-k@users.noreply.github.com>
2024-02-12 14:01:52 +0300

    Create 2.3.03.js

---
+++ Diving into JavaScript: For Beginners/step02/2.03/2.3.03.js
@@ -0,0 +1,27 @@
+/*
+
+Запросите у пользователя два числа. Затем выведите эти два числа через пробел. 
+
+Sample Input 1:
+
+1
+2
+
+Sample Output 1:
+
+1 2
+
+Sample Input 2:
+
+55
+88
+
+Sample Output 2:
+
+55 88
+
+*/
+
+let n1 = Number(prompt());
+let n2 = Number(prompt());
+console.log(n1, n2);
