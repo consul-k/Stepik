eb8fcb327ecf5f4a9bfea0d4a2e9626c3be1566c
consul-k <138245769+consul-k@users.noreply.github.com>
2024-02-12 14:47:19 +0300

    Create 2.5.06.js

---
+++ Diving into JavaScript: For Beginners/step02/2.05/2.5.06.js
@@ -0,0 +1,32 @@
+/*
+
+Запросите у пользователя число. Выведите последнюю цифру данного числа, используя математический оператор.
+
+Sample Input 1:
+
+18
+
+Sample Output 1:
+
+8
+
+Sample Input 2:
+
+103
+
+Sample Output 2:
+
+3
+
+Sample Input 3:
+
+356
+
+Sample Output 3:
+
+6
+
+*/
+
+let a = Number(prompt());
+console.log(a%10);
