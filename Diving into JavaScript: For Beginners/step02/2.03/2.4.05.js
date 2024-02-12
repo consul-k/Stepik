f1ee85820bb32edc4908f5496b3dfc44dcaf9fe1
consul-k <138245769+consul-k@users.noreply.github.com>
2024-02-12 14:03:15 +0300

    Create 2.4.05.js

---
+++ Diving into JavaScript: For Beginners/step02/2.03/2.4.05.js
@@ -0,0 +1,30 @@
+/*
+
+Запросите у пользователя три числа. Затем выведите сумму этих трех чисел. 
+
+Sample Input 1:
+
+8
+11
+2
+
+Sample Output 1:
+
+21
+
+Sample Input 2:
+
+5
+4
+1
+
+Sample Output 2:
+
+10
+
+*/
+
+let a = Number(prompt());
+let b = Number(prompt());
+let c = Number(prompt());
+console.log(a+b+c);
