37cb3d8a802c8fd8a53536fb2982dc0b7fce16a7
consul-k <138245769+consul-k@users.noreply.github.com>
2024-02-15 01:03:59 +0300

    Create 5.1.02.js

---
+++ Diving into JavaScript: For Beginners/step05/5.01/5.1.02.js
@@ -0,0 +1,36 @@
+/*
+
+Объявите функцию, в которой будут содержаться инструкции запроса числа от пользователя и вывода квадрата этого числа. 
+Вызовите эту функцию.
+
+Sample Input 1:
+
+5
+
+Sample Output 1:
+
+25
+
+Sample Input 2:
+
+9
+
+Sample Output 2:
+
+81
+
+Sample Input 3:
+
+3
+
+Sample Output 3:
+
+9
+
+*/
+
+function call() {
+    let n = Number(prompt());
+    console.log(n**2);
+}
+call();
