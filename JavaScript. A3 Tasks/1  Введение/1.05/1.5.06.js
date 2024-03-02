2b6569803433d579f94ac83ad0ebfc229112cbea
consul-k <138245769+consul-k@users.noreply.github.com>
2024-03-02 14:27:37 +0300

    Create 1.5.06.js

---
+++ JavaScript. A3 Tasks/1  Введение/1.05/1.5.06.js
@@ -0,0 +1,29 @@
+/*
+
+Меньше и больше
+
+Дано число N. Выведите два числа: в первой строке должно быть число меньше N, во второй строке число больше N.
+
+Sample Input 1:
+
+100
+
+Sample Output 1:
+
+95
+173
+
+Sample Input 2:
+
+-10
+
+Sample Output 2:
+
+-55
+31
+
+*/
+
+let N = Number(prompt())
+console.log(N-10);
+console.log(N+10);
