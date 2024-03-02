db4fd9f40fdc63d46a9b30f589026d811d8969f7
consul-k <138245769+consul-k@users.noreply.github.com>
2024-03-02 14:25:57 +0300

    Create 1.5.02.js

---
+++ JavaScript. A3 Tasks/1  Введение/1.05/1.5.02.js
@@ -0,0 +1,18 @@
+/*
+
+Получите две строки и выведите их в одну строку.
+
+Sample Input:
+
+Hello
+World
+
+Sample Output:
+
+Hello World
+
+*/
+
+let a = prompt();
+let b = prompt();
+console.log(a,b);
