445bbaec30e44bb7456ab0bfccec2a7fd6843380
consul-k <138245769+consul-k@users.noreply.github.com>
2024-03-02 14:26:21 +0300

    Create 1.5.03.js

---
+++ JavaScript. A3 Tasks/1  Введение/1.05/1.5.03.js
@@ -0,0 +1,21 @@
+/*
+
+Напишите программу, которая запрашивает три буквы и выводит строку из этих букв в том же порядке, что и при вводе.
+
+Sample Input:
+
+A
+B
+C
+
+Sample Output:
+
+ABC
+
+*/
+
+let A = prompt();
+let B = prompt();
+let C = prompt();
+
+console.log(A+B+C);
