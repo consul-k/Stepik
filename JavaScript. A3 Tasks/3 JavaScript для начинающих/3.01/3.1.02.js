309dd5def184ed4880de48c25d7ee85c81e5efa7
consul-k <138245769+consul-k@users.noreply.github.com>
2024-03-06 12:34:13 +0300

    Create 3.1.02.js

---
+++ JavaScript. A3 Tasks/3 JavaScript для начинающих/3.01/3.1.02.js
@@ -0,0 +1,14 @@
+/*
+
+В указанное место напишите код, который поместит значение из переменной a в переменную x.
+
+*/
+
+function testA(a) {         
+    let x = "";
+    x = a;
+    return x;
+}        
+
+const a = prompt()
+console.log(testA(a))
