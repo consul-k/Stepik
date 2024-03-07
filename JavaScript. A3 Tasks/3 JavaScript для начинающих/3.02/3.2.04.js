45236ca797a5264caf70fe3d539820df9e9cdf0f
consul-k <138245769+consul-k@users.noreply.github.com>
2024-03-07 13:36:40 +0300

    Create 3.2.04.js

---
+++ JavaScript. A3 Tasks/3 JavaScript для начинающих/3.02/3.2.04.js
@@ -0,0 +1,23 @@
+/*
+
+В этом задании в нашу функцию testStr передаются две строки. Вам нужно вернуть из функции строку, которая будет включать в себя обе этих строки, 
+преобразовав в первой строке все буквы в заглавные, а во второй - в строчные.
+
+Sample Input:
+
+Hello
+World!
+
+Sample Output:
+
+HELLOworld!
+
+*/
+
+function testStr(a, b) {
+    return a.toUpperCase() + b.toLowerCase();
+}
+
+let a = prompt()
+let b = prompt()
+console.log(testStr(a, b))
