ac585a0c1155e9a21d68d1e62bbb894c2ab2eee6
consul-k <138245769+consul-k@users.noreply.github.com>
2024-03-22 01:40:02 +0300

    Create 3.3.01.py

---
+++ Coding and Learning: Python/step03/3.03/3.3.01.py
@@ -0,0 +1,11 @@
+n = int(input())
+if 0 <= n <= 59:
+    print('F')
+elif 60 <= n <= 69:
+    print('D')
+elif 70 <= n <= 79:
+    print('C')
+elif 80 <= n <= 89:
+    print('B')
+else:
+    print('A')
