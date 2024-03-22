b4f8c4cbf5da5af38614dca3887ae78d05d47bc3
consul-k <138245769+consul-k@users.noreply.github.com>
2024-03-22 14:00:58 +0300

    Create 3.3.05.py

---
+++ Coding and Learning: Python/step03/3.03/3.3.05.py
@@ -0,0 +1,10 @@
+month = int(input())
+
+if month == 12 or month == 1 or month == 2:
+    print("Зима")
+elif month == 3 or month == 4 or month == 5:
+    print("Весна")
+elif month == 6 or month == 7 or month == 8:
+    print("Лето")
+else:
+    print("Осень")
