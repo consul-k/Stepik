e418017c6ff896e3c30832deb8b6c60dfdf17369
consul-k <138245769+consul-k@users.noreply.github.com>
2024-03-23 14:47:23 +0300

    Create 2.5.02.py

---
+++ Good Python/step02/2.05/2.5.02.py
@@ -0,0 +1,5 @@
+pervaia_strochka=  input();vtoraya_strochka=input();chetviortaia_strochka      =     input();
+tretia_strochka =     input();pataia_strochka     =    input();
+#все это жесть
+print(pervaia_strochka,vtoraya_strochka,tretia_strochka,chetviortaia_strochka,  sep = 'n');
+print(pataia_strochka);
