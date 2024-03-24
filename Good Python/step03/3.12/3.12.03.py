ecf3212b85dea72a27e28d1c341675461222ecb7
consul-k <138245769+consul-k@users.noreply.github.com>
2024-03-25 00:00:51 +0300

    Create 3.12.03.py

---
+++ Good Python/step03/3.12/3.12.03.py
@@ -0,0 +1,9 @@
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+sum_of_abs = abs(num1) + abs(num2) + abs(num3)
+abs_of_sum = abs(num1 + num2 + num3)
+
+print(sum_of_abs)
+print(abs_of_sum)
