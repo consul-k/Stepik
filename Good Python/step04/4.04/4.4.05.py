e448ceb1ed09aacf968d39bb6ca0d9618a0a11f2
consul-k <138245769+consul-k@users.noreply.github.com>
2024-03-25 00:07:12 +0300

    Create 4.4.05.py

---
+++ Good Python/step04/4.04/4.4.05.py
@@ -0,0 +1,7 @@
+n = int(input())
+if n==1 or n==3 or n==5 or n==7 or n==9 or n==12 or n==14 or n==16 or n==18 or n==19 or n==21 or n==23 or n==25 or n== 27 or n==30 or n==32 or n==34 or n==36:
+    print ('красный')
+elif n==0:
+    print ('зеленый')
+else:
+    print ('черный')
