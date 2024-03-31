ddfbbcbefc3e596bf42d543922697b3d7c0abd1d
consul-k <138245769+consul-k@users.noreply.github.com>
2024-04-01 02:00:52 +0300

    Create 10.3.02.py

---
+++ Good Python/step10/10.03/10.3.02.py
@@ -0,0 +1,5 @@
+from random import choice
+
+card = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'валет', 'дама', 'король', 'туз']
+color = ['червей', 'бубно', 'треф', 'пик']
+print(choice(card),choice(color))
