675a2f1fcda981346a6d1baed61caa5ea4afe81c
consul-k <138245769+consul-k@users.noreply.github.com>
2024-04-08 13:04:48 +0300

    Create 5.1.01.sql

---
+++ Introduction to SQL/step05/5.01/5.1.01.sql
@@ -0,0 +1,8 @@
+CREATE TABLE readers
+(
+    reader_num INT AUTO_INCREMENT,
+    reader_name VARCHAR(100),
+    reader_adress VARCHAR(100),
+    reader_phone VARCHAR(20) NOT NULL,
+    PRIMARY KEY(reader_num)
+);
