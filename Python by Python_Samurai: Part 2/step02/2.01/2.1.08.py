439631dc3efb33d82b103b72ec360321ea9f6348
consul-k <138245769+consul-k@users.noreply.github.com>
2024-04-29 14:02:23 +0300

    Create 2.1.08.py

---
+++ Python by Python_Samurai: Part 2/step02/2.01/2.1.08.py
@@ -0,0 +1,17 @@
+lst = [
+    {
+     'id': 1, 'name': 'fruits',
+     'list': ['apples', 'bananas', 'oranges']
+    },
+    {
+     'id': 2, 'name': 'vegetables',
+     'lst': ['potato', 'tomatoes', 'cucumbers']
+    },
+    {
+     'id': 3, 'name': 'milk products',
+     'sp': ['milk', 'ice cream', 'cheese']
+    }
+]       
+# продолжите решение здесь
+res = [lst[0]['list'], lst[1]['lst'], lst[2]['sp']]
+print(res)
