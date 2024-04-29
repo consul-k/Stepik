d706f127864d15c355aca8bcfb6480536ce76d33
consul-k <138245769+consul-k@users.noreply.github.com>
2024-04-29 14:00:54 +0300

    Create 2.1.03.py

---
+++ Python by Python_Samurai: Part 2/step02/2.01/2.1.03.py
@@ -0,0 +1,9 @@
+workers = {
+    'emp_1': {'name': 'Garry', 'salary': 9000},
+    'emp_2': {'name': 'Ron', 'salary': 6000},
+    'emp_3': {'name': 'Dobby', 'salary': 500}
+}        
+
+# продолжите решение здесь
+workers['emp_3']['salary'] = 700
+print(workers)
