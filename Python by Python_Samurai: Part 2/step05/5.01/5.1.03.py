56799a7f578b654675cfbb5a5ad4a862728d7d94
consul-k <138245769+consul-k@users.noreply.github.com>
2024-05-05 00:30:55 +0300

    Create 5.1.03.py

---
+++ Python by Python_Samurai: Part 2/step05/5.01/5.1.03.py
@@ -0,0 +1,7 @@
+def func(num1, num2):
+    if num1 % 2 == 0 and num2 % 2 == 0:
+        print(num1 + num2)
+    elif num1 % 2 != 0 and num2 % 2 != 0:
+        print(num1 - num2)
+    else:
+        print(num1 + num2)
