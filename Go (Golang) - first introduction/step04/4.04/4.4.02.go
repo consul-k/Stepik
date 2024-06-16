1a9b1e74ce03e6473a416e1dbedee1785f8464a2
consul-k <138245769+consul-k@users.noreply.github.com>
2024-06-16 15:51:13 +0300

    fix 4.4.02.go

--- Go (Golang) - first introduction/step04/4.04/4.4.02.go
+++ Go (Golang) - first introduction/step04/4.04/4.4.02.go
@@ -7,7 +7,7 @@ func main() {
     fmt.Scan(&height)
     if height > 185 {
         fmt.Println("Высокий рост")
-    } else if height >= 170 || height <= 185 {
+    } else if height >= 170 && height <= 185 {
         fmt.Println("Средний рост")
     } else {
         fmt.Println("Небольшой рост")
