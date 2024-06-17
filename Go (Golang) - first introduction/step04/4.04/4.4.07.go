dcdf712985cefa6809543d1dd9f7765f6717ddbe
consul-k <138245769+consul-k@users.noreply.github.com>
2024-06-17 14:24:33 +0300

    Create 4.4.07.go

---
+++ Go (Golang) - first introduction/step04/4.04/4.4.07.go
@@ -0,0 +1,80 @@
+package main
+
+import "fmt"
+
+func main() {
+    var a, b, c int
+    fmt.Scan(&a, &b, &c)
+
+    if a == 0 {
+        fmt.Println("Ноль")
+    } else if a == 1 {
+        fmt.Println("Один")
+    } else if a == 2 {
+        fmt.Println("Два")
+    } else if a == 3 {
+        fmt.Println("Три")
+    } else if a == 4 {
+        fmt.Println("Четыре")
+    } else if a == 5 {
+        fmt.Println("Пять")
+    } else if a == 6 {
+        fmt.Println("Шесть")
+    } else if a == 7 {
+        fmt.Println("Семь")
+    } else if a == 8 {
+        fmt.Println("Восемь")
+    } else if a == 9 {
+        fmt.Println("Девять")
+    } else if a == 10 {
+        fmt.Println("Десять")
+    }
+
+    if b == 0 {
+        fmt.Println("Ноль")
+    } else if b == 1 {
+        fmt.Println("Один")
+    } else if b == 2 {
+        fmt.Println("Два")
+    } else if b == 3 {
+        fmt.Println("Три")
+    } else if b == 4 {
+        fmt.Println("Четыре")
+    } else if b == 5 {
+        fmt.Println("Пять")
+    } else if b == 6 {
+        fmt.Println("Шесть")
+    } else if b == 7 {
+        fmt.Println("Семь")
+    } else if b == 8 {
+        fmt.Println("Восемь")
+    } else if b == 9 {
+        fmt.Println("Девять")
+    } else if b == 10 {
+        fmt.Println("Десять")
+    }
+
+    if c == 0 {
+        fmt.Println("Ноль")
+    } else if c == 1 {
+        fmt.Println("Один")
+    } else if c == 2 {
+        fmt.Println("Два")
+    } else if c == 3 {
+        fmt.Println("Три")
+    } else if c == 4 {
+        fmt.Println("Четыре")
+    } else if c == 5 {
+        fmt.Println("Пять")
+    } else if c == 6 {
+        fmt.Println("Шесть")
+    } else if c == 7 {
+        fmt.Println("Семь")
+    } else if c == 8 {
+        fmt.Println("Восемь")
+    } else if c == 9 {
+        fmt.Println("Девять")
+    } else if c == 10 {
+        fmt.Println("Десять")
+    }
+}
