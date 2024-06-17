4d7f11f4df17587bd40109687147c54ad32ca9a6
consul-k <138245769+consul-k@users.noreply.github.com>
2024-06-17 13:46:17 +0300

    Create 4.4.04.go

---
+++ Go (Golang) - first introduction/step04/4.04/4.4.04.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+// здесь вам нужно написать свой код
+    var a, b, c int
+    fmt.Scanln(&a, &b, &c)
+    fmt.Println(a + b + c)
+    fmt.Println(a * b * c)
+}
