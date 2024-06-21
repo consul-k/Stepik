a727424635efa6b9e3076c1c5d217ce8e0de6358
consul-k <138245769+consul-k@users.noreply.github.com>
2024-06-21 13:38:13 +0300

    Create 5.4.02.php

---
+++ PHP - first introduction/step05/5.04/5.4.02.php
@@ -0,0 +1,10 @@
+<?php
+// put your PHP code here
+    $a = readline();
+    $b = readline();
+    $c = readline();
+    $d = readline();
+    $ind = readline();
+    $myarr = array($a, $b, $c, $d);
+    echo $myarr[$ind];
+?>
