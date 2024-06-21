6392083b7e3008b0d9851ba0c965491c0d4df27f
consul-k <138245769+consul-k@users.noreply.github.com>
2024-06-21 22:29:21 +0300

    Create 6.9.01.php

---
+++ PHP - first introduction/step06/6.09/6.9.01.php
@@ -0,0 +1,16 @@
+<?php
+$i = readline();
+switch ($i) {
+  case 1:
+    echo "Зима";  
+    break;
+  case 2:
+    echo "Весна";
+    break;
+  case 3:
+    echo "Лето";
+    break;
+  case 4:
+    echo "Осень";
+    break;
+}
