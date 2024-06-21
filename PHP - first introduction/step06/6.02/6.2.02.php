ac9d86804bbf875c687f7656b0bc21da752d4a88
consul-k <138245769+consul-k@users.noreply.github.com>
2024-06-21 14:16:48 +0300

    Create 6.2.02.php

---
+++ PHP - first introduction/step06/6.02/6.2.02.php
@@ -0,0 +1,13 @@
+<?php
+// put your PHP code here
+    $a = readline();
+    if ($a == "Великобритания") {
+        echo "Лондон";
+    } elseif ($a == "Испания") {
+        echo "Мадрид";
+    } else if ($a == "Китай") {
+        echo "Пекин";
+    } else {
+        echo "Это неправильная страна";
+    }
+?>
