c4ea738db261cece9c0d2b3486931d235439f9ee
consul-k <138245769+consul-k@users.noreply.github.com>
2024-06-21 13:42:39 +0300

    Create 5.4.03.php

---
+++ PHP - first introduction/step05/5.04/5.4.03.php
@@ -0,0 +1,10 @@
+<?php
+// put your PHP code here
+    $name1 = readline();
+    $age1 = readline();
+    $name2 = readline();
+    $age2 = readline();
+    $name3 = readline();
+    $age3 = readline();
+    $asarr = array($name1 => $age1, $name2 => $age2, $name3 => $age3);
+?>
