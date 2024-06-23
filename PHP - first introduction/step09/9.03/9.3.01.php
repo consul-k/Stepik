c1fb26951ba405285099939880189d3c8b05c944
consul-k <138245769+consul-k@users.noreply.github.com>
2024-06-23 15:00:00 +0300

    Create 9.3.01.php

---
+++ PHP - first introduction/step09/9.03/9.3.01.php
@@ -0,0 +1,12 @@
+class Car {
+    public $model;
+    public $color;
+    public $wheels;
+    public $speed;
+    public function sayHello() {
+        echo "Я машина";
+    }
+}
+//пишите свой код ниже
+class Truck extends Car {
+}
