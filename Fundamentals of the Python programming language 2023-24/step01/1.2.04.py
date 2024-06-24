c2cc2ccba4d84f879df11dce2585be40b1ea448e
consul-k <138245769+consul-k@users.noreply.github.com>
2024-06-24 14:57:04 +0300

    Create 1.2.04.py

---
+++ Fundamentals of the Python programming language 2023-24/step01/1.2.04.py
@@ -0,0 +1,11 @@
+n = int(input())
+if n == 2:
+    print('неудовлетворительно')
+elif n == 3:
+    print('удовлетворительно')
+elif n == 4:
+    print('хорошо')
+elif n == 5:
+    print('отлично')
+else:
+    print(n)
