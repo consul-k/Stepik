8175829ba5bd64726875b8b563a359040a3b547f
consul-k <138245769+consul-k@users.noreply.github.com>
2024-07-07 13:33:20 +0300

    Create 4.1.08.py

---
+++ "Python Generation": a course for beginners/step04/4.01/4.1.08.py
@@ -0,0 +1,9 @@
+n = int(input())
+if n <= 13:
+    print('детство')
+elif 14 <= n and n <= 24:
+    print('молодость')
+elif 25 <= n and n <= 59:
+    print('зрелость')
+elif n >= 60:
+    print('старость')
