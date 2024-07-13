940eb01bc676e7f628d1da057282da211d3067d4
consul-k <138245769+consul-k@users.noreply.github.com>
2024-07-13 15:33:29 +0300

    Create 13.5.04.py

---
+++ "Python Generation": a course for beginners/step13/13.05/13.5.04.py
@@ -0,0 +1,15 @@
+def is_password_good(password):
+    if len(password) < 8:
+        return False
+
+    has_upper = any(char.isupper() for char in password)
+    has_lower = any(char.islower() for char in password)
+    has_digit = any(char.isdigit() for char in password)
+
+    return has_upper and has_lower and has_digit
+
+# считываем данные
+txt = input()
+
+# вызываем функцию
+print(is_password_good(txt))
