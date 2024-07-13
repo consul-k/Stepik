8a3a8b63c582c406f7233d040d6f30aa8184de7d
consul-k <138245769+consul-k@users.noreply.github.com>
2024-07-13 15:19:52 +0300

    Create 13.5.02.py

---
+++ "Python Generation": a course for beginners/step13/13.05/13.5.02.py
@@ -0,0 +1,16 @@
+# объявление функции
+def is_prime(num):
+    res = [1]
+    for i in range(2,num+1):
+        if num%i==0:
+            res.append(i)
+    if len(res) == 2:
+        return True
+    else:
+        return False
+
+# считываем данные
+n = int(input())
+
+# вызываем функцию
+print(is_prime(n))
