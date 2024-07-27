f3ed7f88fcbc6a377e607befde5c5749e0a2df77
consul-k <138245769+consul-k@users.noreply.github.com>
2024-07-27 13:26:44 +0300

    Create 14.1.03.py

---
+++ "Python Generation": a course for beginners/step14/14.1.03.py
@@ -0,0 +1,12 @@
+# объявление функции
+from math import *
+
+def compute_binom(n, k):
+    return int(factorial(n)/(factorial(k)*factorial(n-k)))
+
+# считываем данные
+n = int(input())
+k = int(input())
+
+# вызываем функцию
+print(compute_binom(n, k))
