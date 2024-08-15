90cde869b549b4049a97e6c90ff77ccf95eb908d
consul-k <138245769+consul-k@users.noreply.github.com>
2024-08-15 13:24:37 +0300

    Create 7.3.04.py

---
+++ Python summer 24/step07/7.03/7.3.04.py
@@ -0,0 +1,15 @@
+def is_one_away(word1, word2):
+    if len(word1) == len(word2):
+        dif = 0
+        for i in range(len(word1)):
+            if word1[i] != word2[i]:
+                dif += 1
+        return dif == 1
+    return False
+
+# считываем данные
+txt1 = input()
+txt2 = input()
+
+# вызываем функцию
+print(is_one_away(txt1, txt2))
