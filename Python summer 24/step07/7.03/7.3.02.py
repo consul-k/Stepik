fdae1287e42cf68d4d0ae785a1107d6f220cdefd
consul-k <138245769+consul-k@users.noreply.github.com>
2024-08-15 13:04:52 +0300

    Create 7.3.02.py

---
+++ Python summer 24/step07/7.03/7.3.02.py
@@ -0,0 +1,13 @@
+# объявление функции
+def is_prime(num):
+    res = [1]
+    for i in range(2,num+1):
+        if num%i==0:
+            res.append(i)
+    if len(res) == 2:
+        return True
+    else:
+        return False
+
+n = int(input())
+print(is_prime(n))
