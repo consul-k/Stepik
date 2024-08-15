6e85be9a600870e96ff6387567a874f1483a4caf
consul-k <138245769+consul-k@users.noreply.github.com>
2024-08-15 14:59:46 +0300

    Create 7.4.01.py

---
+++ Python summer 24/step07/7.04/7.4.01.py
@@ -0,0 +1,9 @@
+def get_middle_point(x1, y1, x2, y2):
+    print((x1 + x2)/2, (y1 + y2)/2)
+
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+
+get_middle_point(x1, y1, x2, y2)
