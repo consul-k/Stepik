5a4ccca6324d01841cf664cc7bf466d9188bd2d7
consul-k <138245769+consul-k@users.noreply.github.com>
2024-08-24 13:09:16 +0300

    Create 7.4.04.py

---
+++ Python summer 24/step07/7.04/7.4.04.py
@@ -0,0 +1,9 @@
+def draw_triangle():
+    height = 8
+    width = 15
+    for i in range(height):
+        stars = '*' * (2 * i + 1)
+        spaces = ' ' * (height - i - 1)
+        print(spaces + stars)
+        
+draw_triangle()
