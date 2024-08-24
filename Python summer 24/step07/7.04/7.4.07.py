e093f8e982b78ae7e581413f714d5d6b38145460
consul-k <138245769+consul-k@users.noreply.github.com>
2024-08-24 13:12:03 +0300

    Create 7.4.07.py

---
+++ Python summer 24/step07/7.04/7.4.07.py
@@ -0,0 +1,9 @@
+def is_magic(date):
+    day, month, year = map(int, date.split('.'))
+    return day * month == int(str(year)[-2:])
+    
+# считываем данные
+date = input()
+
+# вызываем функцию
+print(is_magic(date))
