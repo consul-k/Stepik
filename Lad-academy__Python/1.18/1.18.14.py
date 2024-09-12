3ff5145132aa03cc4c8fd9296412299709876ebe
consul-k <138245769+consul-k@users.noreply.github.com>
2024-09-12 12:15:08 +0300

    Create 1.18.14.py

---
+++ Lad-academy__Python/1.18/1.18.14.py
@@ -0,0 +1,4 @@
+words = ['Plum', 'Grapefruit', 'apple', 'orange', 'pomegranate', 'Cranberry', 'lime', 'Lemon', 'grapes', 'persimmon', 'tangerine', 'Watermelon', 'currant', 'Almond']
+
+result = {l[0].lower() for l in words}
+print(*sorted(result))
