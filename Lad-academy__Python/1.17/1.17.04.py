33c96c407634beadcab6a83d72c55a98fc78d0c2
consul-k <138245769+consul-k@users.noreply.github.com>
2024-09-20 13:36:12 +0300

    Create 1.17.04.py

---
+++ Lad-academy__Python/1.17/1.17.04.py
@@ -0,0 +1,31 @@
+pets = [('Hatiko', 'Parker', 'Wilson', 50),
+        ('Rusty', 'Josh', 'King', 25),
+        ('Fido', 'John', 'Smith', 28),
+        ('Butch', 'Jake', 'Smirnoff', 18),
+        ('Odi', 'Emma', 'Wright', 18),
+        ('Balto', 'Josh', 'King', 25),
+        ('Barry', 'Josh', 'King', 25),
+        ('Snape', 'Hannah', 'Taylor', 40),
+        ('Horry', 'Martha', 'Robinson', 73),
+        ('Giro', 'Alex', 'Martinez', 65),
+        ('Zooma', 'Simon', 'Nevel', 32),
+        ('Lassie', 'Josh', 'King', 25),
+        ('Chase', 'Martha', 'Robinson', 73),
+        ('Ace', 'Martha', 'Williams', 38),
+        ('Rocky', 'Simon', 'Nevel', 32)]
+
+result = {}
+
+for k in pets:
+    key = []
+    value = []
+    for i in k[-3:]:
+        key.append(i)
+    if tuple(key) in result:
+        value = result[tuple(key)]
+        value.append(k[-4])
+        result[tuple(key)] = value
+    else:
+        result[tuple(key)] = [k[-4]]
+
+print(result)
