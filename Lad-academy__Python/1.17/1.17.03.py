0029d497b9e48c9630e06f935f98a0ba1b8d9792
consul-k <138245769+consul-k@users.noreply.github.com>
2024-09-20 13:04:25 +0300

    Create 1.17.03.py

---
+++ Lad-academy__Python/1.17/1.17.03.py
@@ -0,0 +1,4 @@
+text = 'footballcyberpunkextraterritorialityconversationalistblockophthalmoscopicinterdependencemamauserfff'
+
+result = dict(zip([i for i in text], [text.count(i) for i in text]))
+print(result)
