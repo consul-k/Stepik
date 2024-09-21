3e94f30c21c8427a47f2bf1809e7ec0c45aae29e
consul-k <138245769+consul-k@users.noreply.github.com>
2024-09-21 12:38:13 +0300

    Create 1.17.06.py

---
+++ Lad-academy__Python/1.17/1.17.06.py
@@ -0,0 +1,13 @@
+s = 'orange strawberry barley gooseberry apple apricot barley currant orange melon pomegranate banana banana orange barley apricot plum grapefruit banana quince strawberry barley grapefruit banana grapes melon strawberry apricot currant currant gooseberry raspberry apricot currant orange lime quince grapefruit barley banana melon pomegranate barley banana orange barley apricot plum banana quince lime grapefruit strawberry gooseberry apple barley apricot currant orange melon pomegranate banana banana orange apricot barley plum banana grapefruit banana quince currant orange melon pomegranate barley plum banana quince barley lime grapefruit pomegranate barley'
+
+result = dict(zip([i for i in s.split()], [s.count(i) for i in s.split()]))
+m = max(result.values())
+list_m = []
+
+for word in result:
+    if result[word] == m:
+        list_m.append(word)
+        
+list_m = sorted(list_m)
+
+print(list_m[0])
