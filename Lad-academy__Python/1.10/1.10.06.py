ca71ece883bb511706dcb993fcd124d103128954
consul-k <138245769+consul-k@users.noreply.github.com>
2024-10-10 12:16:35 +0300

    Create 1.10.06.py

---
+++ Lad-academy__Python/1.10/1.10.06.py
@@ -0,0 +1,4 @@
+keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'try', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'while', 'yield']
+
+res = [i for i in keywords if len(i) >= 5]
+print(res)
