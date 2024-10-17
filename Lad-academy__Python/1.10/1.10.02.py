f63e1b930f261a3868f244530733d76c138075c0
consul-k <138245769+consul-k@users.noreply.github.com>
2024-10-17 16:08:56 +0300

    Create 1.10.02.py

---
+++ Lad-academy__Python/1.10/1.10.02.py
@@ -0,0 +1,18 @@
+def compress_list(nums):
+    non_zero_index = 0
+
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[non_zero_index] = nums[i]
+            non_zero_index += 1
+
+    for i in range(non_zero_index, len(nums)):
+        nums[i] = 0
+
+    return nums
+
+nums = list(map(int, input().split()))
+
+result = compress_list(nums)
+
+print(" ".join(map(str, result)))
