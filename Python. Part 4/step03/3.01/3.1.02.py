1d42c0e95389c264c2c2f9702d2dab1fe7e488da
consul-k <138245769+consul-k@users.noreply.github.com>
2024-11-08 15:01:54 +0300

    Update 3.1.02.py

--- Python. Part 4/step03/3.01/3.1.02.py
+++ Python. Part 4/step03/3.01/3.1.02.py
@@ -1,7 +1,7 @@
 def positive(func):
     def wrapper(*args):
         result = func(*args)
-        if result > 0:
+        if result >= 0:
             return result
         else:
             return 0
