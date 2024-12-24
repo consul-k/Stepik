f34adb142ec74b36863053a573f54579c6528ae8
consul-k <138245769+consul-k@users.noreply.github.com>
2024-12-24 14:40:35 +0300

    Create 2.6.06.py

---
+++ Cognizant Python - Base/step02/2.06/2.6.06.py
@@ -0,0 +1,9 @@
+# put your python code here
+student = []
+student.append(["Политология"])
+student.append(["Физкультура"])
+student.append(["География"])
+student[0].extend([5,5,5])
+student[1].extend([3,3,3])
+student[2].extend([4,4,4])
+print(student)
