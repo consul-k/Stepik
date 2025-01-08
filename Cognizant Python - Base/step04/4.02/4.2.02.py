39cd1d94547b0a69f9c7b37708e836b5fad95beb
consul-k <138245769+consul-k@users.noreply.github.com>
2025-01-08 14:31:56 +0300

    Create 4.2.02.py

---
+++ Cognizant Python - Base/step04/4.02/4.2.02.py
@@ -0,0 +1,11 @@
+my_list = [10, 20, 30, 40]
+my_iterator = iter(my_list)
+print(next(my_iterator))  
+print(next(my_iterator)) 
+
+# код выше оставьте как есть, ваша задача дописать программу:
+my_iterator = iter(my_list)
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
