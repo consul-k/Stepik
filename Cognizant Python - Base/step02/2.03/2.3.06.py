f3b07e5bc7c7756fa69e6d72e8c08c055bf27eec
consul-k <138245769+consul-k@users.noreply.github.com>
2025-01-09 14:04:57 +0300

    Create 2.3.06.py

---
+++ Cognizant Python - Base/step02/2.03/2.3.06.py
@@ -0,0 +1,13 @@
+storage1 = 10
+storage2 = 20
+
+storage1 -= moving1
+storage2 += moving1
+
+storage2 -= moving2
+storage1 += moving2
+
+storage1 -= moving3
+storage2 += moving3
+
+print(storage1, storage2)
