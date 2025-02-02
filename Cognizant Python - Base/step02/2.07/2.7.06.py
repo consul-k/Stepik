610b1f94626160292cd73fd235424ac22fb0451d
consul-k <138245769+consul-k@users.noreply.github.com>
2025-02-02 16:14:18 +0300

    Create 2.7.06.py

---
+++ Cognizant Python - Base/step02/2.07/2.7.06.py
@@ -0,0 +1,6 @@
+# Переменная exchange_rate уже создана волшебным образом
+max_num = max(exchange_rate, default='неизвестно')
+min_num = min(exchange_rate, default='неизвестно')
+
+print(f'Максимальное значение валюты: {max_num}')
+print(f'Минимальное значение валюты: {min_num}')
