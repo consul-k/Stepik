4c2c89921c0e7b7f0f6170f989a128e993d6be56
consul-k <138245769+consul-k@users.noreply.github.com>
2025-02-08 16:55:18 +0300

    Create 2.8.04.py

---
+++ Indie Python Programming Course/step02/2.08/2.8.04.py
@@ -0,0 +1,8 @@
+day1 = float(input())
+day2 = float(input())
+day3 = float(input())
+
+average_rate = (day1 + day2 + day3) / 3
+
+# Вывод результата с двумя знаками после запятой
+print(f"Средний курс доллара за последние три дня: {average_rate:.2f} RUB.")
