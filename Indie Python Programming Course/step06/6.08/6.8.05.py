3fed21d1c46a61f22972e19a76a461c36c82ab2e
consul-k <138245769+consul-k@users.noreply.github.com>
2025-02-24 14:33:58 +0300

    Create 6.8.05.py

---
+++ Indie Python Programming Course/step06/6.08/6.8.05.py
@@ -0,0 +1,19 @@
+persons= [
+    ('Allison Hill', 334053, 'M', '1635644202'),
+    ('Megan Mcclain', 191161, 'F', '2101101595'),
+    ('Brandon Hall', 731262, 'M', '6054749119'), 
+    ('Michelle Miles', 539898, 'M', '1355368461'),
+    ('Donald Booth', 895667, 'M', '7736670978'), 
+    ('Gina Moore', 900581, 'F', '7018476624'),
+    ('James Howard', 460663, 'F', '5461900982'), 
+    ('Monica Herrera', 496922, 'M', '2955495768'),
+    ('Sandra Montgomery', 479201, 'M', '5111859731'), 
+    ('Amber Perez', 403445, 'M', '0602870126')
+]
+data = {}
+for person in persons:
+    data[person[0]] = {}
+    for _ in person:
+        data[person[0]]['salary'] = person[1]
+        data[person[0]]['gender'] = person[2]
+        data[person[0]]['passport'] = person[3]
