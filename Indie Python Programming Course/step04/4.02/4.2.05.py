603754cb4b03b445f23f41493124bacd960a52e6
consul-k <138245769+consul-k@users.noreply.github.com>
2025-02-26 14:12:20 +0300

    Create 4.2.05.py

---
+++ Indie Python Programming Course/step04/4.02/4.2.05.py
@@ -0,0 +1,12 @@
+def syracuse_sequence_steps(n):
+    steps = 0
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+        steps += 1
+    return steps
+
+n = int(input())
+print(syracuse_sequence_steps(n))
