8c93932f1984ea6e10013b01a4d9105d0d880417
consul-k <138245769+consul-k@users.noreply.github.com>
2025-03-02 15:42:20 +0300

    Create 2.15.04.py

---
+++ Indie Python Programming Course/step02/2.15/2.15.04.py
@@ -0,0 +1,8 @@
+numbers = [-214, 181, -139, 448, -20, -917, 32, 422, -895, 198, 284, 472, -986, -964, -989, 29]
+s = 0
+s += numbers.pop()
+s += numbers.pop(0)
+s += numbers.pop(12)
+s += numbers.pop(7)
+print(numbers)
+print(s)
