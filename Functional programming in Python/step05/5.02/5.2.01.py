b69900d41a158983af941c9638a284cc8503d3c7
consul-k <138245769+consul-k@users.noreply.github.com>
2025-03-23 17:05:08 +0300

    Create 5.2.01.py

---
+++ Functional programming in Python/step05/5.02/5.2.01.py
@@ -0,0 +1,12 @@
+exchange_rates = {
+    "USD": 1.0,
+    "EUR": 0.861775,
+    "GBP": 0.726763,
+    "INR": 75.054725,
+    "AUD": 1.333679,
+    "CAD": 1.237816,
+    "SGD": 1.346851,
+}
+
+def convert(first, second, n):
+    return round((exchange_rates[second]/exchange_rates[first]) * n, 2)
