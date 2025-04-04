eb6272dfa77c48ae4880dc5aa68256adcd71264f
consul-k <138245769+consul-k@users.noreply.github.com>
2025-04-04 14:52:54 +0300

    Create 5.5.03.py

---
+++ Functional programming in Python/step05/5.05/5.5.03.py
@@ -0,0 +1,7 @@
+def print_goods(lst):
+    pass
+
+create_attrs(print_goods, [('house', 1), ('level', 3), ('cost', 1000000)])
+print(print_goods.house)
+print(print_goods.level)
+print(print_goods.cost)
