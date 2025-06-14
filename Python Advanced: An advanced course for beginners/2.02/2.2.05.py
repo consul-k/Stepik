be9fef40eef2078a3eb87f0af324f37bb04b44e2
consul-k <138245769+consul-k@users.noreply.github.com>
2025-06-14 12:42:13 +0300

    Create 2.2.05.py

---
+++ Python Advanced: An advanced course for beginners/2.02/2.2.05.py
@@ -0,0 +1,11 @@
+desyatikom_num, polovinnik_num = map(float, input().split())
+
+desyatikom_result = desyatikom_num * 2
+polovinnik_result = polovinnik_num / 2
+
+if desyatikom_result > polovinnik_result:
+    print("Победил Десятиком!")
+elif desyatikom_result < polovinnik_result:
+    print("Победил Половинник!")
+else:
+    print("Ничья! Числа равны.")
