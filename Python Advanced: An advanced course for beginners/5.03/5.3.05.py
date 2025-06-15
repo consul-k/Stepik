d13d6d6c81bf8e7ae58f0b59834d4de1b3a315f5
consul-k <138245769+consul-k@users.noreply.github.com>
2025-06-15 15:21:45 +0300

    Create 5.3.05.py

---
+++ Python Advanced: An advanced course for beginners/5.03/5.3.05.py
@@ -0,0 +1,17 @@
+# данные лошадей
+horses = (("Черный ворон", 45), ("Стальной конь", 50), ("Буря", 48))
+
+# здесь продолжите программу
+horse_name = input()
+distance = int(input())
+
+for i in horses:
+    if horse_name in i:
+        speed = i[1]
+        time_hours = distance / speed
+        hours = int(time_hours)
+        minutes = round((time_hours - hours) * 60)
+        print(f"Лошадь {horse_name} пройдет {distance} км за {hours} часов и {minutes} минут.")
+        break
+else:
+    print("Лошадь с таким именем не найдена.")
