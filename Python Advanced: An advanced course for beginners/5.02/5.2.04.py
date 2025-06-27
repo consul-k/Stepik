1645c7df8b321ddebf988aff5e31ee548cb91f74
consul-k <138245769+consul-k@users.noreply.github.com>
2025-06-27 14:03:28 +0300

    Create 5.2.04.py

---
+++ Python Advanced: An advanced course for beginners/5.02/5.2.04.py
@@ -0,0 +1,17 @@
+# кортеж с данными о магических городах
+cities = ('Эльдорадо', 'Люминор', 'Арканум')
+populations = (5000, 7000, 12000)
+magics = ('Земная магия', 'Воздушная магия', 'Водная магия')
+
+# здесь продолжите программу
+a = (input(),)
+b = (int(input()),)
+c = (input(),)
+
+cities += a
+populations += b
+magics += c
+
+print(cities)
+print(populations)
+print(magics)
