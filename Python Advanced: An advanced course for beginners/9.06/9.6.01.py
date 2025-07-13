352651517be576de337979b932829d5db72ac0a3
consul-k <138245769+consul-k@users.noreply.github.com>
2025-07-13 13:20:25 +0300

    Create 9.6.01.py

---
+++ Python Advanced: An advanced course for beginners/9.06/9.6.01.py
@@ -0,0 +1,11 @@
+scroll1 = input().split()
+scroll2 = input().split()
+
+set1 = set(scroll1)
+set2 = set(scroll2)
+common = frozenset(set1 & set2)
+
+# Вывод с подписями
+print(f"Уникальные руны первого свитка: {sorted(set1)}")
+print(f"Уникальные руны второго свитка: {sorted(set2)}")
+print(f"Защищённый свиток (общие руны): frozenset({sorted(common)})")
