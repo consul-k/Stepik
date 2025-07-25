f79225ac0329040a5eeb019fe18ab5f881f951ad
consul-k <138245769+consul-k@users.noreply.github.com>
2025-07-25 14:16:48 +0300

    Create 5.4.03.py

---
+++ Python Advanced: An advanced course for beginners/5.04/5.4.03.py
@@ -0,0 +1,18 @@
+data = input()
+
+stones = data.split(',')
+
+stone_power = {}
+
+for stone in stones:
+    stone = stone.strip()
+    stone_type, power = stone.rsplit(' ', 1)
+    power = int(power)
+
+    if stone_type in stone_power:
+        stone_power[stone_type] += power
+    else:
+        stone_power[stone_type] = power
+
+for stone_type, total_power in stone_power.items():
+    print(f"Общая сила {stone_type} камень: {total_power}")
