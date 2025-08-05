7c97cea9cf054ef08314f76711db140a6ee60582
consul-k <138245769+consul-k@users.noreply.github.com>
2025-08-05 12:21:08 +0300

    Create 5.1.01.py

---
+++ Python | Second Step | Advanced Concepts/5.01/5.1.01.py
@@ -0,0 +1,11 @@
+pupil_john = {
+    "name": "John",
+    "age": 25,
+    "city": "London",
+    "is_student": True
+}
+
+pupil_john['age'] = 28
+pupil_john['is_student'] = False
+
+print(pupil_john)
