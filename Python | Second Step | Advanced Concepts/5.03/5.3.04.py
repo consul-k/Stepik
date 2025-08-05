04895959ae0776d5cb719f610d4af427a5aebccd
consul-k <138245769+consul-k@users.noreply.github.com>
2025-08-05 14:12:26 +0300

    Create 5.3.04.py

---
+++ Python | Second Step | Advanced Concepts/5.03/5.3.04.py
@@ -0,0 +1,10 @@
+inventory = {
+    "apple": 10,
+    "banana": 5,
+    "orange": 7,
+    "grapes": 3,
+    "melon": 2
+}
+
+print(inventory.get("banana"))
+print(inventory.get("pear", "Not found"))
