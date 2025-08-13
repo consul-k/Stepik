84db7df02659df963b5aa73bb61a7a0bce76cda6
consul-k <138245769+consul-k@users.noreply.github.com>
2025-08-13 14:23:05 +0300

    Create 8.1.01.py

---
+++ Adaptive Python Trainer/ 8. Interpreters/8.1.01.py
@@ -0,0 +1,12 @@
+i = input().split()
+a = int(i[0])
+b = int(i[2])
+s = i[1]
+if s == 'plus':
+    print(a + b)
+if s == 'minus':
+    print(a - b)
+if s == 'multiply':
+    print(a * b)
+if s == 'divide':
+    print(a // b)
