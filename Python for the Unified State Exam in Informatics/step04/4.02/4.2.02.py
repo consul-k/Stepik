96c35a96b721b0fd36299898a03eae1c167a8523
consul-k <138245769+consul-k@users.noreply.github.com>
2025-08-28 13:55:16 +0300

    Create 4.2.02.py

---
+++ Python for the Unified State Exam in Informatics/step04/4.02/4.2.02.py
@@ -0,0 +1,8 @@
+# put your python code here
+year = int(input())  # Вводим год
+
+# Проверка условий високосности года
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+    print("YES")
+else:
+    print("NO")
