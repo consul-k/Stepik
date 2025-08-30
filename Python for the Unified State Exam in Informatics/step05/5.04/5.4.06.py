017bb5f1d25d62ed26e0fbddae5fd079c6754d07
consul-k <138245769+consul-k@users.noreply.github.com>
2025-08-30 13:13:38 +0300

    Create 5.4.06.py

---
+++ Python for the Unified State Exam in Informatics/step05/5.04/5.4.06.py
@@ -0,0 +1,8 @@
+q=0
+for i1 in range(10):
+    for i2 in range(10):
+        for i3 in range(10):
+            for i4 in range(10):
+                if i1==i2 and i3==i4:
+                    q+=1
+print(q)
