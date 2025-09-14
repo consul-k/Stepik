df84f5ed78721a6f0b51a23c552c1456530a55bb
consul-k <138245769+consul-k@users.noreply.github.com>
2025-09-14 12:10:16 +0300

    Create 2.6.01.py

---
+++ PROcode: Advanced Python Course/step02/2.06/2.6.01.py
@@ -0,0 +1,11 @@
+lst = ['нож', 'пистолет', 'фонарь', 'крест', 'рукоять от меча']
+
+a, b, c = input(), input(), input()
+if a not in lst:
+    lst.append(a)
+if b not in lst:
+    lst.append(b)
+if c not in lst:
+    lst.append(c)
+
+print('Снаряжение обновлено:',lst)
