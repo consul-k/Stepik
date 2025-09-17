30816161561e06ae21717d2a4f83f92b44b2a208
consul-k <138245769+consul-k@users.noreply.github.com>
2025-09-17 12:18:20 +0300

    Create 5.2.04.py

---
+++ PROcode: Advanced Python Course/step05/5.02/5.2.04.py
@@ -0,0 +1,15 @@
+cities = ('Эльдорадо', 'Люминор', 'Арканум')
+populations = (5000, 7000, 12000)
+magics = ('Земная магия', 'Воздушная магия', 'Водная магия')
+
+a = (input(),)
+b = (int(input()),)
+c = (input(),)
+
+cities += a
+populations += b
+magics += c
+
+print(cities)
+print(populations)
+print(magics)
