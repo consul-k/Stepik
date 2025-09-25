a9bdcc3f1fc4d984af3ad99e85429d606dba402d
consul-k <138245769+consul-k@users.noreply.github.com>
2025-09-25 12:50:40 +0300

    Create 9.2.02.py

---
+++ PROcode: Advanced Python Course/step09/9.02/9.2.02.py
@@ -0,0 +1,16 @@
+input_data = input()
+
+resources = set(resource.strip() for resource in input_data.split(';') if resource.strip())
+unique_count = len(resources)
+most_valuable = max(resources, key=len)
+rarest = min(resources, key=len)
+
+total_value = sum(len(resource) for resource in resources)
+
+sorted_resources = sorted(resources, key=len)
+
+print(f"\nОбщее количество уникальных ресурсов: {unique_count}")
+print(f"Самый ценный ресурс: {most_valuable}")
+print(f"Самый редкий ресурс: {rarest}")
+print(f"Суммарная ценность ресурсов: {total_value}")
+print(f"Ресурсы в отсортированном порядке: {', '.join(sorted_resources)}")
