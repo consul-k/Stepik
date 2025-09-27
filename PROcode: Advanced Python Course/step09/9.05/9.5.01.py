0124a20e5f6ee658e6e25e94c1f2d547a30156a6
consul-k <138245769+consul-k@users.noreply.github.com>
2025-09-27 13:32:53 +0300

    Create 9.5.01.py

---
+++ PROcode: Advanced Python Course/step09/9.05/9.5.01.py
@@ -0,0 +1,9 @@
+N = int(input())
+A = set(input().split())
+
+M = int(input())
+B = set(input().split())
+
+print(f"A is subset of B? {A.issubset(B)}")
+print(f"B is superset of A? {B.issuperset(A)}")
+print(f"A and B are disjoint? {A.isdisjoint(B)}")
