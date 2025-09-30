10e4511856b5379208578a11f198158453438967
consul-k <138245769+consul-k@users.noreply.github.com>
2025-09-30 13:36:54 +0300

    Create 11.2.01.py

---
+++ PROcode: Advanced Python Course/step11/11.02/11.2.01.py
@@ -0,0 +1,10 @@
+data = list(map(int, input().split()))
+
+core_data = data[0:5]
+signal1 = data[5]
+signal2 = data[6]
+signal3 = data[7]
+signal4 = data[8]
+activation_code = data[9]
+
+print(*core_data)
