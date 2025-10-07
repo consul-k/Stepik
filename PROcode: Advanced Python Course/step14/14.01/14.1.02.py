9a311f1d84e90e4e5d76cc80a832c8b50833e89a
consul-k <138245769+consul-k@users.noreply.github.com>
2025-10-07 13:27:14 +0300

    Create 14.1.02.py

---
+++ PROcode: Advanced Python Course/step14/14.01/14.1.02.py
@@ -0,0 +1,12 @@
+filename = input().strip()
+
+# далее ваш код
+file = open(filename, 'r')
+content = file.read()
+print(content)
+file.close()
+
+file = open(filename, 'r')
+line = file.readline()
+print(line)
+file.close()
